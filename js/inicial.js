2127bb80df0bcd1f98adef740b3bf98a8a661d4b
Arthur <arthur.felipe1993@gmail.com>
2025-06-18 02:08:18 -0300

    Criação da inicial

---
+++ js/inicial.js
@@ -0,0 +1,23 @@
+const categorias = [
+  { nome: 'Alimentos', icone: '../assets/icon/alimentos.png', rota: 'alimentos.html' },
+  { nome: 'Sentimentos', icone: '../assets/icon/sentimentos.png', rota: 'sentimentos.html' },
+  { nome: 'Brincar', icone: '../assets/icon/brincar.png', rota: 'brincar.html' },
+  { nome: 'Pessoas', icone: '../assets/icon/pessoas.png', rota: 'pessoas.html' },
+  { nome: 'Necessidades', icone: '../assets/icon/necessidades.png', rota: 'necessidades.html' },
+  { nome: 'Locais', icone: '../assets/icon/locais.png', rota: 'locais.html' },
+];
+
+const grid = document.querySelector('.grid');
+
+categorias.forEach(categoria => {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.onclick = () => window.location.href = categoria.rota;
+
+  card.innerHTML = `
+    <img src="${categoria.icone}" alt="${categoria.nome}">
+    <p>${categoria.nome}</p>
+  `;
+
+  grid.appendChild(card);
+});
